a8e853a2289872732bcbcd913cc12ec556f1d24b
Jonathan <jonathanelian64@gmail.com>
2025-05-09 13:46:44 -0700

    feat: add cors support

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 import os
+from flask_cors import CORS
 
 load_dotenv()
 
 DB_URL = os.getenv("database_url")
 app = Flask(__name__)
+CORS(app)
 
 def get_db_conn():
     conn = psycopg2.connect(
@@ -33,6 +35,9 @@ def get_books():
     cursor.execute("SELECT * FROM book order by id")
     books = cursor.fetchall()
     conn.close()
+
+    def get(self):
+        return repo.get_all(), 200, {'Access-Control-Allow-Origin': '*'}
     return {
         "ok": True,
         "books": books
